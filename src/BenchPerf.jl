15eafa4a9e6876cfdf32e652e62057f31143867e
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-30 16:48:38 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/BenchPerf.jl
@@ -0,0 +1,11 @@
+baremodule BenchPerf
+
+module Internal
+
+using ..BenchPerf: BenchPerf
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule BenchPerf
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BenchPerf
+using Test
+
+@testset "BenchPerf.jl" begin
+    # Write your tests here.
+end
