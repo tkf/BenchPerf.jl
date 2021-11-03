61644451042e0f5b8a4e85d26dbc6611553aab6c
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-03 19:33:44 -0400

    Don't try nthreads > Sys.CPU_THREADS

--- examples/parallel_random_increments/run.jl
+++ examples/parallel_random_increments/run.jl
@@ -1,7 +1,13 @@
 using BenchmarkConfigSweeps
 
+if Sys.CPU_THREADS < 8
+    threads = [1, Sys.CPU_THREADS]
+else
+    threads = [1, 4, 8]
+end
+
 BenchmarkConfigSweeps.run(
     joinpath(@__DIR__, "build"),
     joinpath(@__DIR__, "benchmarks.jl"),
-    BenchmarkConfigSweeps.nthreads.([1, 4, 8]),
+    BenchmarkConfigSweeps.nthreads.(threads),
 )
