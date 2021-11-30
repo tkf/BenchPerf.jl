b5f82f5f05b8f787fffa30bd00e03f4d88df7322
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-30 13:24:14 -0500

    Add more accessors

--- src/accessors.jl
+++ src/accessors.jl
@@ -6,11 +6,21 @@ const RATIO_TABLE = [
     ("LLC-load-misses", "LLC-loads"),
     ("dTLB-load-misses", "dTLB-loads"),
     ("iTLB-load-misses", "iTLB-loads"),
+    ("stalled-cycles-frontend", "cycles"),
+    ("stalled-cycles-backend", "cycles"),
+]
+
+const OTHER_KEYS = [
+    # "task-clock"  TODO: float
+    "context-switches",
+    "cpu-migrations",
+    "page-faults",
+    "instructions",
 ]
 
 as_property_symbol(str::AbstractString) = Symbol(lowercase(replace(str, "-" => "_")))
 
-const ALL_KEYS = vcat(first.(RATIO_TABLE), last.(RATIO_TABLE))
+const ALL_KEYS = unique!(vcat(first.(RATIO_TABLE), last.(RATIO_TABLE), OTHER_KEYS))
 
 for key in ALL_KEYS
     val = Val{as_property_symbol(key)}
@@ -28,7 +38,13 @@ for (num, den) in RATIO_TABLE
     end
 end
 
-const ALL_NAMES = (as_property_symbol.(ALL_KEYS)...,)
+# perf calls them (and the ratios) "shaddow stats"
+const DERIVED_NAMES = [
+    :instructions_per_cycle,  # "insn per cycle"
+    :stalled_cycles_per_instructions,  # "stalled cycles per insn"
+]
+
+const ALL_NAMES = (as_property_symbol.(ALL_KEYS)..., DERIVED_NAMES...)
 const RATIO_NAMES = (as_property_symbol.(first.(RATIO_TABLE))...,)
 
 struct RatioAccessor
@@ -58,3 +74,18 @@ end
 
 getratio(::TrialPerf, @nospecialize(_val::Val{name})) where {name} =
     error(name, " is not a name of suportd ratio ", RATIO_NAMES)
+
+function getvalue(trial::TrialPerf, ::Val{:instructions_per_cycle})
+    num = @something(trial.instructions, return)
+    den = @something(trial.cycles, return)
+    return num / den
+end
+
+# https://github.com/torvalds/linux/blob/v5.15/tools/perf/util/stat-shadow.c#L979-L991
+function getvalue(trial::TrialPerf, ::Val{:stalled_cycles_per_instructions})
+    f = @something(trial.stalled_cycles_frontend, return)
+    b = @something(trial.stalled_cycles_backend, return)
+    num = max(f, b)
+    den = @something(trial.instructions, return)
+    return num / den
+end
